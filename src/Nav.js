93c58d23157d5e47b352c350a59dcc64fb3b7e2d
enkronan <robin@tempura.se>
2020-05-28 19:55:38 +0200

    type: feature
    added styling to nav react-links

--- src/Nav.js
+++ src/Nav.js
@@ -4,22 +4,24 @@ import {Link} from 'react-router-dom'
 
 function Nav() {
 
-    
+    const navStyle = {
+        color: 'white'
+    };
 
 
-  return (
-    <nav>
-        <h3>logo</h3>
-        <ul className="nav-links">
-            <Link to='/about'>
-            <li>About</li>
-            </Link>
-            <Link to='/shop'>
-            <li>Shop</li>
-            </Link>
-        </ul>
-    </nav>
-  );
+    return (
+        <nav>
+            <h3>logo</h3>
+            <ul className="nav-links">
+                <Link style={navStyle} to='/about'>
+                <li>About</li>
+                </Link>
+                <Link style={navStyle} to='/shop'>
+                <li>Shop</li>
+                </Link>
+            </ul>
+        </nav>
+    );
 }
 
 export default Nav;
