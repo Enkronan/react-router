98b57ba743e625605db191f1d31fe016ac4fe919
enkronan <robin@tempura.se>
2020-05-28 20:35:00 +0200

    type: feature
    added a basic API call which displays first 10 hits

--- src/shop.js
+++ src/shop.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function Shop() {
-  return (
-    <div>
-      <h1>Shop</h1>
-    </div>
-  );
+
+    const APP_ID = '4767b429';
+    const APP_KEY = 'f2a1a0856ea66ddd0525fd7b405d3ded';
+
+    const [items, setItems] = useState([]);
+    const [query, setQuery] = useState("chicken");
+    
+
+    useEffect(() => {
+        fetchItems();
+    }, []);
+
+    const fetchItems = async () => {
+        const data = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+        const items = await data.json();
+        console.log(items.hits);
+        setItems(items.hits)
+    }
+
+    return (
+        <div>
+        {items.map(item => (
+            <h1 key={item.recipe.label}>{item.recipe.label}</h1>
+        ))}
+        </div>
+    );
 }
 
 export default Shop;
